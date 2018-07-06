36a54be3ba450efc7509a977adbc08d9f7aa7685
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2018-07-06 17:24:38 +0300

    emitting 2 other versions light raw and light minified

--- src/lib-usage.js
+++ src/lib-usage.js
@@ -1,4 +1,4 @@
-import { GostCoding } from './crypto/gostCoding';
+import { GostCoding, gostCodingInstance as coding } from './crypto/gostCoding';
 import { GostSign } from './engine/gostSign';
 import CryptoGostAPI from './crypto-gost-wrapper';
 
@@ -13,7 +13,9 @@ console.log('Hash test result: ', hashTest);
 /* verify test */
 const publicKey = 'KM0R5bn0+pAaQPdse9AYECNUInRL1RqH6R6UlgdsQgahm7OVFw7fAapvletSpYQcmwzuC5PqVLKqIEcpyC12TA==';
 const signature = 'VBTs9RLwHEwB6lu4b+tlLAuQUakG5VYktiCKHv3bWmt9J1UxgKgzIPKwI8K76Y4EXZlClyWJ1cp4evtLnISxkA==';
-console.log('Verify test result: ', CryptoGostAPI.verify(publicKey, encodedMessage, signature));
+const decodedPublicKey = coding.Base64.decode(publicKey, true);
+const decodedSign = coding.Base64.decode(signature, true);
+console.log('Verify test result: ', CryptoGostAPI.verify(decodedPublicKey, encodedMessage, decodedSign));
 
 /* sign test */
 const cipher = new GostSign(CryptoGostAPI.algorithmGostSign);
